7ff33368969fc0b424110c34a99806370e32c841
KT <thun.kristian@gmail.com>
2024-05-14 13:25:26 +0200

    first init

---
+++ Pet_BnB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pet_BnB
+//
+//  Created by Kristian Thun on 2024-05-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pet_BnB/Pet_BnBApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pet_BnBApp.swift
+//  Pet_BnB
+//
+//  Created by Kristian Thun on 2024-05-14.
+//
+
+import SwiftUI
+
+@main
+struct Pet_BnBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
