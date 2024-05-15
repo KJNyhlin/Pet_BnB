10069fa652f84eaa38ead3922fb6c7d7b92ffe96
Maida <98819095+maidads@users.noreply.github.com>
2024-05-16 00:30:47 +0200

    added function fetchHouses to get the houses from the database

--- Pet_BnB/Helpers/FirebaseHelper.swift
+++ Pet_BnB/Helpers/FirebaseHelper.swift
@@ -10,7 +10,23 @@ import Firebase
 import FirebaseAuth
 import FirebaseFirestore
 import FirebaseStorage
+import FirebaseFirestoreSwift
 
 class FirebaseHelper: ObservableObject {
-    
+    @Published var houses = [House]()
+        private var db = Firestore.firestore()
+        
+        func fetchHouses() {
+            db.collection("houses").addSnapshotListener { (querySnapshot, error) in
+                guard let documents = querySnapshot?.documents else {
+                    print("No documents")
+                    return
+                }
+                
+                self.houses = documents.compactMap { queryDocumentSnapshot -> House? in
+                    return try? queryDocumentSnapshot.data(as: House.self)
+                }
+            }
+        }
 }
+
