631dae8881be609826d11ac3e18ae026b2c6b3be
Maida <98819095+maidads@users.noreply.github.com>
2024-05-22 21:49:51 +0200

    basic view to make it work

--- Pet_BnB/Views/FilterView.swift
+++ Pet_BnB/Views/FilterView.swift
@@ -5,3 +5,40 @@
 //  Created by Maida on 2024-05-22.
 //
 
+import SwiftUI
+
+struct FilterView: View {
+    @Binding var isPresented: Bool
+    @Binding var minBeds: Int
+    @Binding var minSize: Int
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Filter")
+                    .font(.largeTitle)
+                    .padding()
+                Spacer()
+                Button("Done") {
+                    isPresented = false
+                }
+                .padding()
+            }
+            
+            Form {
+                Section(header: Text("Number of Beds")) {
+                    Stepper(value: $minBeds, in: 1...10) {
+                        Text("\(minBeds) beds")
+                    }
+                }
+                
+                Section(header: Text("Size (in sqm)")) {
+                    Stepper(value: $minSize, in: 10...1000, step: 10) {
+                        Text("\(minSize) sqm")
+                    }
+                }
+            }
+            Spacer()
+        }
+    }
+}
