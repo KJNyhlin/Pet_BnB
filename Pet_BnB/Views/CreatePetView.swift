f71e8f2891dd39063e713f1f3f3aff1dd5a10f5d
KJNyhlin <karlnyhlin@me.com>
2024-05-26 12:48:04 +0200

    can drag and expand pet picture, also minimize virtual keyboard in CreatePetView

--- Pet_BnB/Views/CreatePetView.swift
+++ Pet_BnB/Views/CreatePetView.swift
@@ -16,63 +16,156 @@ struct CreatePetView: View {
     @State private var isImagePickerPresented = false
     @State private var isSaving = false
     @State private var showAlert = false
+    @State private var imagePosition = CGSize.zero
+    @State private var lastImagePosition = CGSize.zero
+    @State private var imageScale: CGFloat = 1.0
+    @State private var lastImageScale: CGFloat = 1.0
+
+    @FocusState private var isNameFocused: Bool
+    @FocusState private var isDescriptionFocused: Bool
 
     var body: some View {
         VStack {
-            HStack(spacing: 0) {
-                VStack {
-                    ZStack {
-                        if let image = vm.image {
+            VStack {
+                ZStack {
+                    if let image = vm.image {
+                        GeometryReader { geometry in
                             Image(uiImage: image)
                                 .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 150, height: 150)
+                                .aspectRatio(contentMode: .fit)
+                                .offset(x: self.imagePosition.width, y: self.imagePosition.height)
+                                .scaleEffect(self.imageScale)
+                                .frame(width: geometry.size.width, height: geometry.size.height)
                                 .clipShape(Circle())
+                                .gesture(
+                                    DragGesture()
+                                        .onChanged { value in
+                                            self.imagePosition = CGSize(width: value.translation.width + self.lastImagePosition.width, height: value.translation.height + self.lastImagePosition.height)
+                                        }
+                                        .onEnded { _ in
+                                            self.lastImagePosition = self.imagePosition
+                                        }
+                                )
+                                .gesture(
+                                    MagnificationGesture()
+                                        .onChanged { value in
+                                            self.imageScale = value * self.lastImageScale
+                                        }
+                                        .onEnded { _ in
+                                            self.lastImageScale = self.imageScale
+                                        }
+                                )
                                 .clipped()
                                 .padding(.leading, 20)
-                        } else {
-                            Image(systemName: "pawprint.fill")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 100, height: 100)
-                                .padding()
                         }
+                        .frame(width: 150, height: 150)
+                    } else {
+                        Image(systemName: "pawprint.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .padding()
                     }
-                    .onTapGesture {
-                        isImagePickerPresented = true
-                    }
+                }
+                .onTapGesture {
+                    isImagePickerPresented = true
+                }
+                .padding(.top, 20) // Lägger till toppmarginal för att undvika statusfältet
 
-                    EntryFields(placeHolder: "Name", promt: "", field: $vm.name)
+                EntryFields(placeHolder: "Name", promt: "", field: $vm.name)
+                    .focused($isNameFocused) // Sätt fokus på namnfältet
 
-                    Picker("Species", selection: $vm.selectedSpices) {
-                        ForEach(vm.speciesOptions, id: \.self) { specie in
-                            Text(specie)
-                        }
+                Picker("Species", selection: $vm.selectedSpices) {
+                    ForEach(vm.speciesOptions, id: \.self) { specie in
+                        Text(specie)
                     }
-                    .tint(.black)
-                    .frame(maxWidth: .infinity)
-                    .pickerStyle(MenuPickerStyle())
+                }
+                .tint(.black)
+                .frame(maxWidth: .infinity)
+                .pickerStyle(MenuPickerStyle())
+                .overlay(
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .stroke(AppColors.mainAccent, lineWidth: 3)
+                )
+
+                TextEditor(text: $vm.description)
+                    .focused($isDescriptionFocused)
+                    .frame(maxWidth: .infinity, minHeight: 200, maxHeight: 400)
+                    .padding()
                     .overlay(
                         RoundedRectangle(cornerRadius: 25.0)
                             .stroke(AppColors.mainAccent, lineWidth: 3)
                     )
+                    .padding(.vertical)
 
-                    TextEditor(text: $vm.description)
-                        .frame(maxWidth: .infinity, minHeight: 200, maxHeight: 400)
-                        .padding()
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 25.0)
-                                .stroke(AppColors.mainAccent, lineWidth: 3)
-                        )
-                        .padding(.vertical)
+                Spacer()
 
-                    Spacer()
-
-                    Button(action: {
+                Button(action: {
+                    hideKeyboard() // Lägg till för att minimera tangentbordet innan sparandet
+                    isSaving = true
+                    Task {
+                        if let selectedImage = vm.image {
+                            let croppedImage = cropImage(image: selectedImage, scale: self.imageScale, position: self.imagePosition, frameSize: CGSize(width: 150, height: 150))
+                            vm.uploadPetImage(croppedImage) { url in
+                                vm.imageURL = url
+                                vm.savePet { success in
+                                    if success {
+                                        isSaving = false
+                                        DispatchQueue.main.async {
+                                            self.presentationMode.wrappedValue.dismiss()
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            vm.savePet { success in
+                                if success {
+                                    isSaving = false
+                                    DispatchQueue.main.async {
+                                        self.presentationMode.wrappedValue.dismiss()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }, label: {
+                    if isSaving {
+                        ProgressView() // Visa en laddningsindikator under sparprocessen
+                    } else {
+                        FilledButtonLabel(text: "Save")
+                    }
+                })
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal, 80)
+            .padding(.vertical, 20)
+            .onAppear {
+                if let pet = pet {
+                    vm.name = pet.name
+                    vm.selectedSpices = pet.species
+                    vm.description = pet.description ?? ""
+                    vm.imageURL = pet.imageURL
+                    vm.loadImageFromURL(pet.imageURL ?? "")
+                    vm.pet = pet // Tilldela pet för att redigera
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.isNameFocused = true // Sätt fokus på namnfältet efter en kort fördröjning
+                }
+            }
+            .photosPicker(isPresented: $isImagePickerPresented, selection: $vm.imageSelection, matching: .images, photoLibrary: .shared())
+            .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("Don't you want to save your changes?"),
+                    message: Text("You have unsaved changes."),
+                    primaryButton: .destructive(Text("Discard changes")) {
+                        self.presentationMode.wrappedValue.dismiss()
+                    },
+                    secondaryButton: .default(Text("Save")) {
                         isSaving = true
                         Task {
                             if let selectedImage = vm.image {
-                                vm.uploadPetImage(selectedImage) { url in
+                                let croppedImage = cropImage(image: selectedImage, scale: self.imageScale, position: self.imagePosition, frameSize: CGSize(width: 150, height: 150))
+                                vm.uploadPetImage(croppedImage) { url in
                                     vm.imageURL = url
                                     vm.savePet { success in
                                         if success {
@@ -94,89 +187,58 @@ struct CreatePetView: View {
                                 }
                             }
                         }
-                    }, label: {
-                        if isSaving {
-                            ProgressView() // Visa en laddningsindikator under sparprocessen
-                        } else {
-                            FilledButtonLabel(text: "Save")
-                        }
-                    })
+                    }
+                )
+            }
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: Button(action: {
+                if vm.hasUnsavedChanges(pet: pet, imagePosition: imagePosition, imageScale: imageScale) {
+                    showAlert = true
+                } else {
+                    self.presentationMode.wrappedValue.dismiss()
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.horizontal, 80)
-                .padding(.vertical, 20)
-                .onAppear {
-                    if let pet = pet {
-                        vm.name = pet.name
-                        vm.selectedSpices = pet.species
-                        vm.description = pet.description ?? ""
-                        vm.imageURL = pet.imageURL
-                        vm.loadImageFromURL(pet.imageURL ?? "")
-                        vm.pet = pet // Tilldela pet för att redigera
+            }) {
+                Image(systemName: "chevron.left")
+                    .foregroundColor(.blue)
+            })
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        hideKeyboard()
                     }
                 }
-                .photosPicker(isPresented: $isImagePickerPresented, selection: $vm.imageSelection, matching: .images, photoLibrary: .shared())
-                .alert(isPresented: $showAlert) {
-                    Alert(
-                        title: Text("Don't you want to save your changes?"),
-                        message: Text("You have unsaved changes."),
-                        primaryButton: .destructive(Text("Discard changes")) {
-                            self.presentationMode.wrappedValue.dismiss()
-                        },
-                        secondaryButton: .default(Text("Save")) {
-                            isSaving = true
-                            Task {
-                                if let selectedImage = vm.image {
-                                    vm.uploadPetImage(selectedImage) { url in
-                                        vm.imageURL = url
-                                        vm.savePet { success in
-                                            if success {
-                                                isSaving = false
-                                                DispatchQueue.main.async {
-                                                    self.presentationMode.wrappedValue.dismiss()
-                                                }
-                                            }
-                                        }
-                                    }
-                                } else {
-                                    vm.savePet { success in
-                                        if success {
-                                            isSaving = false
-                                            DispatchQueue.main.async {
-                                                self.presentationMode.wrappedValue.dismiss()
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    )
-                }
-                .navigationBarBackButtonHidden(true)
-                .navigationBarItems(leading: Button(action: {
-                    if vm.hasUnsavedChanges(pet: pet) {
-                        showAlert = true
-                    } else {
-                        self.presentationMode.wrappedValue.dismiss()
-                    }
-                }) {
-                    Image(systemName: "chevron.left")
-                        .foregroundColor(.blue)
-                })
             }
         }
+    }
+
+    private func hideKeyboard() {
+        isNameFocused = false
+        isDescriptionFocused = false
+    }
+
+    private func cropImage(image: UIImage, scale: CGFloat, position: CGSize, frameSize: CGSize) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: frameSize)
+        let croppedImage = renderer.image { context in
+            context.cgContext.translateBy(x: frameSize.width / 2, y: frameSize.height / 2)
+            context.cgContext.scaleBy(x: scale, y: scale)
+            context.cgContext.translateBy(x: -frameSize.width / 2 + position.width / scale, y: -frameSize.height / 2 + position.height / scale)
+            image.draw(in: CGRect(origin: .zero, size: frameSize))
+        }
+        return croppedImage
     }
 }
 
 extension PetsViewModel {
-    func hasUnsavedChanges(pet: Pet?) -> Bool {
+    func hasUnsavedChanges(pet: Pet?, imagePosition: CGSize, imageScale: CGFloat) -> Bool {
         guard let pet = pet else {
-            return false
+            return name.isEmpty == false || description.isEmpty == false || selectedSpices != "Dog" || image != nil
         }
         return name != pet.name ||
                selectedSpices != pet.species ||
                description != pet.description ||
-               image != nil && imageURL != pet.imageURL
+               (image != nil && imageURL != pet.imageURL) ||
+               imagePosition != .zero || imageScale != 1.0
     }
 }
 
